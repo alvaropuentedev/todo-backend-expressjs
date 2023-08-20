a32e6d96e7abe8f1aeee63bbcb2ea6926895cd09
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-08-21 01:27:58 +0200

    test cors *

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express()
 app.use(cors({
-    origin: 'https://todo-frontend-green.vercel.app/',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   }));
